013c5b0391f909a32f4a70757cd8386aee211844
ivanoVucak <145533812+ivanoVucak@users.noreply.github.com>
2023-10-25 09:28:25 +0200

    Add files via upload

---
+++ klasaStringP5/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaStringP5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upiši jedan prirodan i jedan decimalan broj odvojen razmakom: ");
+
+            var znak = Console.ReadLine();
+            var podatak = znak.Split(' ');
+            var x = int.Parse(podatak[0]);
+            var y = double.Parse(podatak[1]);
+
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            
+
+            Console.ReadKey(); 
+        }
+    }
+}
